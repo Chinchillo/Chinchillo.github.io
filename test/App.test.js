d3495bab3d9a81f9a717eb0f47246234392f6740
Katrin Reder <k.r-z@arcor.de>
2020-12-17 14:32:52 +0100

    Revert "websocket extension upgrade"
    This reverts commit 3a4729649fe4ef5d8c977dce6fb79eaecca8ef01.

--- test/App.test.js
+++
@@ -1,8 +0,0 @@
-import { TestScheduler } from "jest"
-import App from "../src/containers/App"
-
-describe('My Test Suite', () => {
-    it('My Test Case', () => {
-        expect(true).toEqual(true);
-    });
-});
