3a4729649fe4ef5d8c977dce6fb79eaecca8ef01
Katrin Reder <k.r-z@arcor.de>
2020-12-17 13:16:12 +0100

    websocket extension upgrade

---
+++ test/App.test.js
@@ -0,0 +1,8 @@
+import { TestScheduler } from "jest"
+import App from "../src/containers/App"
+
+describe('My Test Suite', () => {
+    it('My Test Case', () => {
+        expect(true).toEqual(true);
+    });
+});
