24311ad2f6b0f5f2dce62038adae12ae4f0ba1b7
Katrin Reder <k.r-z@arcor.de>
2020-12-17 16:10:15 +0100

    removed css from app so that tests run

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-//import "bootstrap/dist/css/bootstrap.min.css"
-
-import React from "react";
-import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css"
+import 'react-toastify/dist/ReactToastify.css'
+import "../node_modules/react-vis/dist/style.css";
 import "core-js/stable";
 import "regenerator-runtime/runtime";
-
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./containers/App";
 
 const rootNode = document.getElementById("root");
